85e3c9322df9e550f8e8e9930a48c46772188414
Deblina Das <77379919+DeblinaD@users.noreply.github.com>
2021-09-30 23:32:47 +0530

    Create Array_DeletionAtBeginning.cpp

---
+++ Array_DeletionAtBeginning.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+   int array[10], size, i, pos;
+   
+   cout<<"Enter the size of the array: ";
+   cin>>size;
+   
+   cout<<"Enter the elements in the array: ";
+   for(i=0; i<size; i++)
+   {
+       cin>>array[i];
+   }
+   
+   for(i=0; i<size; i++)
+   {
+       array[i]=array[i+1];
+   }
+   
+   for(i=0; i<size-1; i++)
+   {
+       cout<<array[i]<<" ";
+   }
+    return 0;
+}
